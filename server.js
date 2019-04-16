340d125ccc9d06bf7948ba5b428ac10ffec6ea6e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 10:17:05 -0400

    Creates a server.js file to configure our server and to connect to our MongoDB using mongoose.connect()

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ const server = express();
 
 server.use(express.json());
 
-// need a MongoDB uri to connect with database
+// configures the database with the URI
+const db = require('./config/keys').mongoURI;
+
+// connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => {
+        console.log('The MongoDB is connected!');
+    })
+    .catch(err => {
+        console.log(err)
+    });
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
