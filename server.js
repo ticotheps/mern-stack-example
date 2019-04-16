e5cbbc1f61be3d4863c7335c4b987ea9ab40f0c3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 10:04:57 -0400

    Adds dotenv as a dependency and creates a .gitignore file in the root directory

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose'); // ORM used to communicate with MongoDB
+
+const server = express();
+
+server.use(express.json());
+
+// need a MongoDB uri to connect with database
