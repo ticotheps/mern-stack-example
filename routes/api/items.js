f84a350f6f85e3414d70280b296f3c1b9003d1bd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 10:47:36 -0400

    Creates a route for POST requests to the '/api/items' endpoint, allowing items to be saved to our MongoDB database

--- routes/api/items.js
+++ routes/api/items.js
@@ -9,13 +9,33 @@ const Item = require('../../models/Item');
 // @route   GET request to 'api/items'
 // @desc    Retrieves all items
 // @access  Public 
-
-// GET Request Handler for all requests to 'api/items' endpoint
 router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 }) // sorts all retrieved items by date; "-1" = descending order, "1" = ascending order 
         .then(items => {
             res.json(items)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+// @route   POST request to 'api/items'
+// @desc    Creates an item
+// @access  Public 
+router.post('/', (req, res) => {
+    // 'new' refers to us creating a new instance of the 'Item' model we made in Item.js
+    // 'Item' refers to the 'Item' model (template) we created in Item.js
+    const newItem = new Item({
+        name: req.body.name
+    }); 
+
+     
+    newItem
+        .save() // saves the new item to our database
+        .then(item => {
+            res.json(item);
         })
         .catch(err => {
             res.json(err);
