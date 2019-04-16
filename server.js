480fac4b9348f95e52e8fcbe29fa6122981b89ce
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 10:31:12 -0400

    Creates a new 'routes' folder, that has a new 'api' folder in it, which then has a nested 'items.js' file in it to hold our routes for requests

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose'); // ORM used to communicate with MongoDB
 
+// any requests that go to the '/api/items' endpoint will refer to this folder
+const items = require('./routes/api/items'); 
+
 const server = express();
 
 server.use(express.json());
@@ -18,6 +21,10 @@ mongoose
     .catch(err => {
         console.log(err)
     });
+
+// tells the server to use the routes from the 'items' variable, 
+// which is defined above, for any requests to the '/api/items' endpoint
+server.use('/api/items', items); 
 
 const port = process.env.PORT || 5000;
 
